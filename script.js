d621928fa4dfb6ff631e5d627d424d89f34d3204
upendrakda <upendrakda123@gmail.com>
2025-11-24 21:14:35 +0545

    feat: introduce score board

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ let num = Math.floor(Math.random() * 3);
 const comp_choice = ["Rock", "Paper", "Scissor"];
 document.getElementById("demo").innerHTML = comp_choice[num];
 
+let clicked= 0;
+
 function changeImage(element) {
+     if (clicked) return;
+    clicked = 1;
+
     document.getElementById("user").src = element.src;
     var user_choice = element.alt;
     document.getElementById("demo2").innerHTML = user_choice;
 
     let result="";
+    let comp_score=0;
+    let user_score=0;
     if(user_choice===comp_choice[num]){
         result="Draw!";
     }
-    else if((user_choice === "Rock" && comp_choice === "Scissor") ||
-        (user_choice === "Paper" && comp_choice === "Rock") ||
-        (user_choice === "Scissor" && comp_choice === "Paper")){
+    else if((user_choice === "Rock" && comp_choice[num] === "Scissor") ||
+        (user_choice === "Paper" && comp_choice[num] === "Rock") ||
+        (user_choice === "Scissor" && comp_choice[num] === "Paper")){
         result = "You Win!";
+        user_score++;
     } 
     else {
         result = "You Lose!";
+        comp_score++;
     }
-    document.getElementById("demo3").innerHTML =
-        `You: ${user_choice} <br> Computer: ${comp_choice} <br> Result: ${result}`;
+    document.getElementById("score").innerHTML = `${user_score} - ${comp_score}`;
+    document.getElementById("result").innerHTML = result;
 }
